e2409243d1c9cd4b8e506f04aa6a65229366faa1
khairul-qamar-235 <khairul.q235@gmail.com>
2025-12-16 17:41:01 +0800

    Update med_status.php

--- simulasi data rumah sakit/actions/med_status.php
+++ simulasi data rumah sakit/actions/med_status.php
@@ -1,1 +1,27 @@
+<?php
+include "config/db.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id_resep = $_POST['id_resep'];
+    $status = $_POST['status'];
+
+    $query = "
+        UPDATE resep
+        SET status = '$status'
+        WHERE id_resep = '$id_resep'
+    ";
+
+    if (mysqli_query($koneksi, $query)) {
+        header("Location: pharmacy.php");
+        exit;
+    } else {
+        echo "Gagal memperbarui status obat: " . mysqli_error($koneksi);
+    }
+
+} else {
+    echo "Akses tidak valid.";
+}
+?>
+
 
